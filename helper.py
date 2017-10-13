83c407f2138db5ecc95dfad96e390d335ab5417a
Thodoris Bais <thodoris.bais@gmail.com>
2017-10-14 01:17:17 +0200

    Boom

---
+++ helper.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import json
+import datetime
+
+# Loads anniversaries.json and gets today's anniversary
+def getAnniversary():
+    with open('anniversaries.json') as data_file:
+        data = json.load(data_file)
+
+        mydate = datetime.datetime.now()
+        # %B is the String representation of a moth
+        # %d is the two-digit number of day of the month
+        monthAndDay = mydate.strftime("%B " + "%d")
+
+
+    return data[monthAndDay]
---
+++ tweet.py
@@ -0,0 +1,20 @@
+import tweepy
+import helper
+
+def getApi(config):
+  auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
+  auth.set_access_token(config['access_token'], config['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+
+  config = {}
+  execfile("config.py", config)
+  api = getApi(config)
+
+  tweet = "#today : " + helper.getAnniversary()
+  status = api.update_status(status=tweet)
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+  main()
